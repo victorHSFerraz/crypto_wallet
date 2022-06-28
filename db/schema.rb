1de20d5e7a51609fcd4514358e402ff5f98ede70
victorHSFerraz <victorf178@gmail.com>
2022-06-27 22:45:03 -0300

    formatter

--- db/schema.rb
+++ db/schema.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -10,13 +12,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_06_26_212549) do
-  create_table "coins", force: :cascade do |t|
-    t.string "description"
-    t.string "acronym"
-    t.string "url_image"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+ActiveRecord::Schema[7.0].define(version: 20_220_626_212_549) do
+  create_table 'coins', force: :cascade do |t|
+    t.string 'description'
+    t.string 'acronym'
+    t.string 'url_image'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
   end
-
 end
